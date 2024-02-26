6a4967648162fd68a20f887dbd1ddeddcfbe1b1d
tatianenm <tatianenmlua@hotmail.com>
2024-02-25 21:23:16 -0300

    spring retry

---
+++ src/main/java/br/com/coffeeandit/config/RetryConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.coffeeandit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RetryConfiguration {
+    @Bean
+    public RetryTemplate retryTemplate(){
+        RetryTemplate retryTemplate = new RetryTemplate();
+
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(2000L);
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(5);
+        retryTemplate.setRetryPolicy(simpleRetryPolicy);
+
+        return retryTemplate;
+    }
+}
