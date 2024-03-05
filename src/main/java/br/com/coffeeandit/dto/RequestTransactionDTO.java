392bad095a2b113b8bdc54905d214defe3c2810e
tatianenm <tatianenmlua@hotmail.com>
2024-03-05 12:44:52 -0300

    kafka producer

--- src/main/java/br/com/coffeeandit/dto/RequestTransactionDTO.java
+++ src/main/java/br/com/coffeeandit/dto/RequestTransactionDTO.java
@@ -1,17 +1,23 @@
 package br.com.coffeeandit.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-public class RequestTransactionDTO extends TransactionDTO{
+@ToString
+@Schema(description = "Objeto de transporte para o envio de uma promessa de transação")
+public class RequestTransactionDTO extends TransactionDTO {
 
+    @JsonIgnore
     private SituacaoEnum situacaoEnum;
-
+    @JsonIgnore
     private LocalDateTime data;
 
 }
